47fccf701f6787930e134dcbca524c3566279259
squallli <javamagic@gmail.com>
2016-10-03 20:58:16 +0800

    Initial Commit

---
+++ MenuTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuTest
+//
+//  Created by 宗桓 李 on 2016/10/3.
+//  Copyright © 2016年 squall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
